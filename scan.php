21bb8671a98dbbcdf1b2752c3082280295158d12
Moisés de LIma <moises@objetivawebsites.com.br>
2018-06-08 18:15:29 -0300

    08/06/2018 - Scan Methods with same name of Class

--- scan.php
+++ scan.php
@@ -1,2 +1,64 @@
-<?
+#!/usr/bin/php -q
+<?php
 
+/*{
+"VERSAO": 1.00,
+"AUTHOR": "MOISES DE LIMA",
+"UPDATE": "08/06/2018"
+}*/
+
+$count = 0;
+
+$open = '/home/myproject';
+
+/**
+* FIND METHODS WITH SAME NAME OF OWN CLASS 
+*/
+function classnamemethod($currentFile){
+
+	if(is_file($currentFile)){
+
+		$php = file_get_contents($currentFile);
+
+		preg_match('/class[\s]([a-zA-Z0-9_]+)[\{\s]?/', $php, $preg);
+
+		if(isset($preg[1])){
+
+			preg_match('/function[\s]('.$preg[1].')[\{\s\(]+?/i', $php, $preg);
+
+			if(isset($preg[1])){
+				print $currentFile.PHP_EOL;
+			}
+		}
+	}
+}
+
+function scanphp($open, $count){
+
+	$list = scandir($open);
+
+	$cleanPath = rtrim($open, '/').'/';
+
+	foreach($list as $files){
+
+		if($files != '.' and $files != '..'){
+
+			$currentFile = $cleanPath.$files;
+
+			if(preg_match('/\.php/', $files)){
+
+				$count = $count + 1;
+				classnamemethod($currentFile);
+			}
+
+			// SUBDIR
+			if(is_dir($currentFile)){
+				$count = scanphp($currentFile, $count);
+			}
+		}
+	}
+
+	return $count;
+}
+
+$count = scanphp($open, $count);
