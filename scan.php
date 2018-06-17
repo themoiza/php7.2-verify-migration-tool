e9969c0e4a68c481fcb4d7b0d7f9efcd557889e9
Moisés de LIma <moises@objetivawebsites.com.br>
2018-06-17 17:08:43 -0300

    17/06/2018 - show lines

--- scan.php
+++ scan.php
@@ -2,155 +2,211 @@
 <?php
 
 /*{
-"VERSION": 1.00,
+"VERSION": 1.10,
 "AUTHOR": "MOISES DE LIMA",
-"UPDATE": "08/06/2018"
+"UPDATE": "17/06/2018"
 }*/
 
-$count = 0;
-
-$open = '/home/gestor';
+$open = '/home/yourproject';
 
 /**
-* FIND METHODS WITH SAME NAME OF YOUR CLASS 
+* FIND METHODS WITH SAME NAME OF YOUR CLASS
 */
-function classnamemethod($currentFile, $count){
 
-	if(is_file($currentFile)){
+class Scan{
 
-		$php = file_get_contents($currentFile);
+	private function  __getLines($str){
 
-		preg_match('/class[\s]([a-zA-Z0-9_]+)[\{\s]?/', $php, $preg);
+		return explode(PHP_EOL, $str);
 
-		if(isset($preg[1])){
+	}
 
-			preg_match('/function[\s]('.$preg[1].')[\{\s\(]+?/i', $php, $preg);
+	function classnamemethod($currentFile, $count){
 
-			if(isset($preg[1])){
-				$count = $count + 1;
-				print $count.' - METHODS WITH SAME NAME OF YOUR CLASS :'.$currentFile.PHP_EOL;
+		if(is_file($currentFile)){
+
+			$php = file_get_contents($currentFile);
+
+			$lines = $this->__getLines($php);
+
+			foreach ($lines as $ct => $line){
+
+				preg_match('/class[\s]([a-zA-Z0-9_]+)[\{\s]?/', $line, $preg);
+
+				if(isset($preg[1])){
+
+					preg_match('/function[\s]('.$preg[1].')[\{\s\(]+?/i', $line, $preg);
+
+					if(isset($preg[1])){
+
+						$count = $count + 1;
+
+						print 'LINE: '.($ct + 1).' - METHODS WITH SAME NAME OF YOUR CLASS IN '.$currentFile.PHP_EOL;
+					}
+				}
 			}
 		}
+
+		return $count;
 	}
 
-	return $count;
-}
+	function auto_load($currentFile, $count){
 
-function auto_load($currentFile, $count){
+		if(is_file($currentFile)){
 
-	if(is_file($currentFile)){
+			$php = file_get_contents($currentFile);
 
-		$php = file_get_contents($currentFile);
+			$lines = $this->__getLines($php);
 
-		preg_match('/__autoload/', $php, $preg);
+			foreach ($lines as $ct => $line){
 
-		if(!empty($preg)){
-			$count = $count + 1;
-			print $count.' - __autoload IS DEPRECATED :'.$currentFile.PHP_EOL;
-		}
-	}
+				preg_match('/__autoload/', $line, $preg);
 
-	return $count;
-}
+				if(!empty($preg)){
 
-function fn_each($currentFile, $count){
+					$count = $count + 1;
 
-	if(is_file($currentFile)){
-
-		$php = file_get_contents($currentFile);
-
-		preg_match('/[\n\s\t]+?each[\s\t\n]?\(/', $php, $preg);
-
-		if(!empty($preg)){
-			$count = $count + 1;
-			print $count.' - each() FUNCTION IS DEPRECATED :'.$currentFile.PHP_EOL;
-		}
-	}
-
-	return $count;
-}
-
-function fn_create_function($currentFile, $count){
-
-	if(is_file($currentFile)){
-
-		$php = file_get_contents($currentFile);
-
-		preg_match('/[\s\t]+?create_function[\s\t\n]?\(/', $php, $preg);
-
-		if(!empty($preg)){
-			$count = $count + 1;
-			print $count.' - create_function() FUNCTION IS DEPRECATED :'.$currentFile.PHP_EOL;
-		}
-	}
-
-	return $count;
-}
-
-function fn_gmp_random($currentFile, $count){
-
-	if(is_file($currentFile)){
-
-		$php = file_get_contents($currentFile);
-
-		preg_match('/[\s\t]+?create_function[\s\t\n]?\(/', $php, $preg);
-
-		if(!empty($preg)){
-			$count = $count + 1;
-			print $count.' - gmp_random() FUNCTION IS DEPRECATED :'.$currentFile.PHP_EOL;
-		}
-	}
-
-	return $count;
-}
-
-function fn_read_exif_data($currentFile, $count){
-
-	if(is_file($currentFile)){
-
-		$php = file_get_contents($currentFile);
-
-		preg_match('/[\s\t]+?read_exif_data[\s\t\n]?\(/', $php, $preg);
-
-		if(!empty($preg)){
-			$count = $count + 1;
-			print $count.' - fn_read_exif_data() FUNCTION IS DEPRECATED :'.$currentFile.PHP_EOL;
-		}
-	}
-
-	return $count;
-}
-
-function scanphp($open, $count){
-
-	$list = scandir($open);
-
-	$cleanPath = rtrim($open, '/').'/';
-
-	foreach($list as $files){
-
-		if($files != '.' and $files != '..'){
-
-			$currentFile = $cleanPath.$files;
-
-			if(preg_match('/\.php/', $files)){
-
-				$count = classnamemethod($currentFile, $count);
-				$count = auto_load($currentFile, $count);
-				$count = fn_each($currentFile, $count);
-				$count = fn_create_function($currentFile, $count);
-				$count = fn_gmp_random($currentFile, $count);
-				$count = fn_read_exif_data($currentFile, $count);
-			}
-
-			// SUBDIR
-			if(is_dir($currentFile)){
-				$count = scanphp($currentFile, $count);
+					print 'LINE: '.($ct + 1).' - __autoload IS DEPRECATED IN '.$currentFile.PHP_EOL;
+				}
 			}
 		}
+
+		return $count;
 	}
 
-	return $count;
+	function fn_each($currentFile, $count){
+
+		if(is_file($currentFile)){
+
+			$php = file_get_contents($currentFile);
+
+			$lines = $this->__getLines($php);
+
+			foreach ($lines as $ct => $line){
+
+				preg_match('/[\n\s\t]+?each[\s\t\n]?\(/', $line, $preg);
+
+				if(!empty($preg)){
+
+					$count = $count + 1;
+
+					print 'LINE: '.($ct + 1).' - each() FUNCTION IS DEPRECATED IN '.$currentFile.PHP_EOL;
+				}
+			}
+		}
+
+		return $count;
+	}
+
+	function fn_create_function($currentFile, $count){
+
+		if(is_file($currentFile)){
+
+			$php = file_get_contents($currentFile);
+
+			$lines = $this->__getLines($php);
+
+			foreach ($lines as $ct => $line){
+
+				preg_match('/[\s\t]+?create_function[\s\t\n]?\(/', $line, $preg);
+
+				if(!empty($preg)){
+
+					$count = $count + 1;
+
+					print 'LINE: '.($ct + 1).' - create_function() FUNCTION IS DEPRECATED IN '.$currentFile.PHP_EOL;
+				}
+			}
+		}
+
+		return $count;
+	}
+
+	function fn_gmp_random($currentFile, $count){
+
+		if(is_file($currentFile)){
+
+			$php = file_get_contents($currentFile);
+
+			$lines = $this->__getLines($php);
+
+			foreach ($lines as $ct => $line){
+
+				preg_match('/[\s\t]+?create_function[\s\t\n]?\(/', $line, $preg);
+
+				if(!empty($preg)){
+
+					$count = $count + 1;
+
+					print 'LINE: '.($ct + 1).' - gmp_random() FUNCTION IS DEPRECATED IN '.$currentFile.PHP_EOL;
+				}
+			}
+		}
+
+		return $count;
+	}
+
+	function fn_read_exif_data($currentFile, $count){
+
+		if(is_file($currentFile)){
+
+			$php = file_get_contents($currentFile);
+
+			$lines = $this->__getLines($php);
+
+			foreach ($lines as $ct => $line){
+
+				preg_match('/[\s\t]+?read_exif_data[\s\t\n]?\(/', $line, $preg);
+
+				if(!empty($preg)){
+
+					$count = $count + 1;
+
+					print 'LINE: '.($ct + 1).' - fn_read_exif_data() FUNCTION IS DEPRECATED IN '.$currentFile.PHP_EOL;
+				}
+			}
+		}
+
+		return $count;
+	}
+
+	function scanphp($open, $count){
+
+		$list = scandir($open);
+
+		$cleanPath = rtrim($open, '/').'/';
+
+		foreach($list as $files){
+
+			if($files != '.' and $files != '..'){
+
+				$currentFile = $cleanPath.$files;
+
+				if(preg_match('/\.php/', $files)){
+
+					$count = $this->auto_load($currentFile, $count);
+					$count = $this->classnamemethod($currentFile, $count);
+					$count = $this->fn_each($currentFile, $count);
+					$count = $this->fn_create_function($currentFile, $count);
+					$count = $this->fn_gmp_random($currentFile, $count);
+					$count = $this->fn_read_exif_data($currentFile, $count);
+				}
+
+				// SUBDIR
+				if(is_dir($currentFile)){
+					$count = $this->scanphp($currentFile, $count);
+				}
+			}
+		}
+
+		return $count;
+	}
 }
 
-$count = scanphp($open, $count);
+$report = 'report.md';
+if(!is_file($report)){
+	touch($report);
+}
+
+$scan = new Scan;
+$count = $scan->scanphp($open, 0);
